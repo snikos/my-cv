e0f5b04f210abd0d6cf30794da9c47c89353d7b6
Niko <snikos@yandex.ru>
2020-12-05 14:17:56 +0800

    +

---
+++ precache-manifest.597e0b711aaa1f9dece493ffe799e50d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "7a5fa914efeb5e187e2e",
+    "url": "/my-cv/static/css/main.f0426376.chunk.css"
+  },
+  {
+    "revision": "7a5fa914efeb5e187e2e",
+    "url": "/my-cv/static/js/main.5d248c9b.chunk.js"
+  },
+  {
+    "revision": "609b4c77da02541e8b10",
+    "url": "/my-cv/static/js/runtime~main.35ffd8d8.js"
+  },
+  {
+    "revision": "f327a14221830277297a",
+    "url": "/my-cv/static/css/2.b630b56d.chunk.css"
+  },
+  {
+    "revision": "f327a14221830277297a",
+    "url": "/my-cv/static/js/2.b595590c.chunk.js"
+  },
+  {
+    "revision": "4d6fb2b12523770d15c42af3d54e295f",
+    "url": "/my-cv/index.html"
+  }
+];
