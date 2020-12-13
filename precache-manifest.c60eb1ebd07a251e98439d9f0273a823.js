652e6b28f07f2032acae4e7c9612d9a1353373b7
Niko <snikos@yandex.ru>
2020-12-13 19:32:07 +0800

    test 3d

---
+++ precache-manifest.c60eb1ebd07a251e98439d9f0273a823.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "07878dacf484c48fc68d",
+    "url": "/my-cv/static/css/main.7de6e0a1.chunk.css"
+  },
+  {
+    "revision": "07878dacf484c48fc68d",
+    "url": "/my-cv/static/js/main.3a3bf756.chunk.js"
+  },
+  {
+    "revision": "609b4c77da02541e8b10",
+    "url": "/my-cv/static/js/runtime~main.35ffd8d8.js"
+  },
+  {
+    "revision": "d8702bb89b301c1250f8",
+    "url": "/my-cv/static/css/2.b630b56d.chunk.css"
+  },
+  {
+    "revision": "d8702bb89b301c1250f8",
+    "url": "/my-cv/static/js/2.79d9b4e2.chunk.js"
+  },
+  {
+    "revision": "19a46b6bf803fabcaadcd841c057098f",
+    "url": "/my-cv/index.html"
+  }
+];
